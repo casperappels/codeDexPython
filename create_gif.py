c722ffb0c13b76b9d05e9fa904011b5f5d75b3f8
casperappels <121339018+casperappels@users.noreply.github.com>
2025-05-28 21:15:37 +0200

    completed project and added exeption handeling

--- create_gif.py
+++ create_gif.py
@@ -1,30 +1,42 @@
 import imageio.v3 as iio
 
-filenames = [
-    'team-pic1.png', 
-    'team-pic2.png'
-    ]
+filenames = []
+
 fileNameBase = ""
 
 amount = 0
 
 images = []
 
-print("please make sure that the pictures you want to use follow the correct naming convention: name1.png")
-fileNameBase = input("What is the name of your photo? without the number and the .png " )
-amount = input(f"how many photos are there from which the name starts with {fileNameBase} ")
 
-for filename in filenames:
-    images.append(
-        iio.imread(
-            filename
-        )5
+
+
+
+try:
+    print("please make sure that the pictures you want to use follow the correct naming convention: name1.png")
+    fileNameBase = input("What is the name of your photo? without the number and the .png " )
+    amount = int(input(f"how many photos are there from which the name starts with {fileNameBase} "))
+    
+    for number in range(1,amount+1):
+        filenames.append(f"{fileNameBase}{number}.png")
+
+    for filename in filenames:
+   
+        images.append(
+            iio.imread(
+               filename
+            )
+        )
+
+
+    iio.imwrite(
+        f'{fileNameBase}.gif', 
+        images, 
+        duration = 500, 
+        loop = 0
     )
+except FileNotFoundError:
+    print("this image does not exist!")
 
-iio.imwrite(
-    'team.gif', 
-    images, 
-    duration = 500, 
-    loop = 0
-)
-
+except ValueError:
+    print("please only use numbers!")
